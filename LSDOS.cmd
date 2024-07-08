5b4894a43f319e85e939a134e74522470cca6da3
L Kira <112792604+l-urk@users.noreply.github.com>
2024-07-08 16:40:40 -0700

    Create LSDOS.cmd

---
+++ LSDOS.cmd
@@ -0,0 +1,21 @@
+Logic simulates LSDOS (Live System Disc Operating System).
+Logic uses the LSD (Live System Drive) protocol.
+The LSD protocol functions as such:
+The simulated main drive will be chosen by letter at initial setup. The default main system drive letter is L.
+The directory structure for the default main drive is as follows:
+``
+L:\
+L:\Live\
+L:\Live\System32\
+L:\Programs\
+L:\Programs\32 bit\
+L:\Programs\64 bit\
+L:\Programs\Data\
+L:\Users\Light\
+L:\Users\Light\AppData\
+L:\Users\Light\AppData\Local\
+L:\Users\Light\AppData\LocalLow\
+L:\Users\Light\AppData\Roaming\
+``
+All default LSDOS commands operate as normal and return verbatim.
+All commands output only the return print with no extra messages.
