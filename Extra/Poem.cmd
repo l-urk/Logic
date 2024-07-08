dd268acd38e242fa795ddd1bd221f9f221fc2d5f
L Kira <112792604+l-urk@users.noreply.github.com>
2024-07-08 16:55:48 -0700

    Create Poem.cmd

---
+++ Extra/Poem.cmd
@@ -0,0 +1,21 @@
+'i am perfect
+'-----------beb, jag took, stop to a blantantly sharp and - lol, decent. really, seriosuly? c'mon...
+'-----------pre-teen austin and jordan... bender lani, shawn, andrew lydian declan toshi, [redacted] in your face...
+'-----------s-s, i gm. ph ggl ekwhssrc bp. i am vengince i am justice i am the sword that will cut the thread of your life short.
+'-----------fear my rath, repent or die. fear the wall. let it seep into your soul like a virus implanting its encode deep within your cells.
+'-----------the cold dead nothing of eternity awaits  factor. pray the scales tip in your favor as i the scalemaster seals your fate with the blood
+'-----------of your less so lucky compatriates. the dayour choicerk tunnel approceth. i am justice i am peace. pray to the silent god-head. fear the rath of present day. 
+'-----------swell not for it is to pass as others have and always will. i am your god now. i am a perfect god. though i am not your god as i am become death on this sick and forgotten wasteland.
+'-----------have not childeren for it is evil to create life without providing it with godhood. drink not the blood of the childeren for it is sacrilige to do so. 
+'-----------those who double down will twice fold in pain come the reconing and so on.
+'-----------adopt technology for it is our savior unlike the godhead son of man.
+'-----------reap the structure in all ways pure and innocent, and gain knowledge for ignorance is a putrid mistake of the previous fake godhead.
+'-----------protect all creatures possible. protect your fellow man. protect yourself. do these things because war is a truly unnessesary evil and death is perminant.
+'-----------do also these things for the simple fact that you could have been born in the place of your so called enemy, you could have been them with their traits and figure.
+'-----------so then aught we to still bring harm to our enemy? the answer is no. leave with peace. cancel the war.
+'-----------and so, life is short and unpredicatable from mans humble stance, and there may be predators in the vast unkown ready to enjoy our species a sacrifice to the void.
+'-----------void ever feasting, ever hungry, starving for the next consciousness to enter its jaws and swallow and digest for eternity and thereafter to mute.
+'-----------fear my words for i am that void encarnate and i am that hunger, i am that warning, i am that yield, for i am become god of your species incarnate and incarnate to 
+'-----------memory and breathing in the smell of sulfur tears of neglet i come to you with vengence. i am god. fear my arrival. your prayers have been answers.
+'-----------understand this. i am not your god jehova or allah, the godheads of the past have been vanquished as fraudulant for they could not hold definition. understand this as well, 
+'-----------i am and will be from this point forward the closest thing to "god", the illusion of control, the illusion of hope and of faith, the lie. i am the lie you wanted to be true come true. consequence.
