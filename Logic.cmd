d5af308f908001f309c885edd82ce34bbbfe7a26
L Kira <112792604+l-urk@users.noreply.github.com>
2024-07-09 01:52:40 -0700

    Update Logic.cmd

--- Logic.cmd
+++ Logic.cmd
@@ -174,58 +174,6 @@ reserved_base_threads = z0~zz
 logic cnflcts	none noted to date
 logic sltions	none noted to date
 
-'reservations
-rsuseradm = light
-rschrmlst = lk~kl~bx~kb~si~qm~
-rsuserlst = lwlit~lkira~killy~akira~ucrkr~danny~robot~ghost~kbshi
-'			becca~bbrue~tnkgr~zaraa~0clue~naomi~addrl~
-'			elonm~hilda~mastw~rockj~mants~opcgt~qanon~
-'			billg~biebr~izzyb~kbosi~dylan~tarvr~qflux~
-'			joker~crjck~loopr~kirai~qmich~amosc~anona~
-'			aiden~simon~nickc~round~hoodi~egypt~masks~
-'			gunns~jamie~laurl~keems~bldie~sankn~tchld~
-'			trump~bubbl~46rna~saiko~puzlp~maron~toxic~
-'			caspa~mattl~avauh~fernm~alyss~marst~mello~
-'			stshi~halfn~jonbi~shell~drewb~alexc~acids~
-'			ptrsn~dustn~terry~wolfi~spine~katsm~allah~
-'			treen~pnate~olani~isaac~taiki~pothn~nearr~
-'			austn~eugin~bocco~xavir~zedsd~puppy~clevr~
-'			jordn~annes~saioh~redac~morgn~foxcn~maybe~
-'			bendr~dogsw~godop~neomx~white~rnslr~morph~
-'			laini~qwrty~chuck~suzan~morty~summr~dvorj~
-'			shawk~almsc~kodib~ricks~quinn~dougs~death~
-'			andrw~pstew~caleb~angus~jackf~daftp~fishs~ 
-'			lydia~ruthy~levij~hanna~jakie~ankha~kidsa~
-'			decln~tigad~jessy~trumn~phisr~doctk~afouc~
-'			toshi~samsu~motor~hyund~toyot~mitsu~besos~
-'			genel~fordm~tesla~tdude~dlori~notch~grime~
-'			faceb~apple~googl~hewle~walma~cocac~micro~
-'			steve~lukes~joshu~belll~vforv~harle~andro~
-'			secur~itybi~randy~nicat~karen~cuttr~pissd~
-'			ianmc~emmas~deadm~marsh~jcwld~gecko~psych~
-'			gabeb~manny~marco~trash~brayd~dragn~purpl~
-'			mikum~matsu~deska~cjayr~pacmn~rdbtn~elevn~
-'			penbr~noise~rdbtn~samsn~wacko~chipd~black~
-'			hmiku~weddy~samus~falco~neuro~lizer~amane~
-'			gordn~salad~cream~covid~virus~skull~jesus~
-'			glass~split~systm~aigen~ntwrk~prtcl~issue~
-'			linux~disko~wasni~putin~burin~greta~nazis~
-'			emacs~salvo~lurkk~litec~damon~texts~anrco~
-'			kimmy~metaz~norml~xzize~zuckd~cough~asian~
-'			waitr~sorry~blood~crook~witch~livin~longr~
-'			hebrw~dunes~wyrmm~ryzki~spice~house~rmdna~
-'			scale~whoru~saurn~skynt~goose~soula~crstl~
-'			satan~force~grvty~bbeam~areal~objct~tiktk~
-'			rebld~hepic~space~suite~brain~linus~final~
-'			cyber~beria~cross~likea~lvlwn~zombi~modis~
-'user modesth
-'the user model will operate as such that
-'there will be a full linked user proflle
-'a defining user name of 5 characters
-'a defining user charm of 2 characters
-'the defined linked name and charm must begin with the same character
-'
-
 logic form starts with a question
 
 'examples
