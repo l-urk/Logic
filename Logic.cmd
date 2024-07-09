2182b0371850f485dfbdfbf70054ebd19ceb2f56
L Kira <112792604+l-urk@users.noreply.github.com>
2024-07-09 01:55:11 -0700

    Update Logic.cmd

--- Logic.cmd
+++ Logic.cmd
@@ -140,36 +140,6 @@ defi
 		replit		=	rplt	=	yes
 		neglit		=	nglt	=	non
 
-'nonreserved_threads
-'reserved_system-threads
-system_threads = 
-a0~az
-b0~bz
-c0~cz
-d0~dz
-e0~ez
-reserved_flag_threads = f0~fz
-g0~gz
-h0~hz
-i0~iz
-j0~jz
-k0~kz
-reserved_live_threads = l0~lz
-m0~mz
-n0~nz
-o0~oz
-p0~pz
-reserved_qtum_threads = q0~qz
-reserved_rand_threads = r0~rz
-reserved_syst_threads = s0~sz
-reserved_time_threads = t0~tz
-reserved_user_threads = u0~uz
-v0~vz
-w0~wz
-reserved_real_threads = x0~xz
-reserved_side_threads = y0~yz
-reserved_base_threads = z0~zz
-
 'logic-notes
 logic cnflcts	none noted to date
 logic sltions	none noted to date
