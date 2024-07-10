16d061b664c4c1c57774e24d91d1d430f7b4d7fa
L Kira <112792604+l-urk@users.noreply.github.com>
2024-07-09 23:06:24 -0700

    Update and rename Logic.cmd to logic.cmd

--- logic.cmd
+++ logic.cmd
@@ -127,19 +127,5 @@ exec
 		vrbs
 		vrbs	(-v)		returns printout at a specified verbosity level defined from 0 to z, unspecified for maximum
 			syntax			;;;; ;;;; -v ;
-the user shares userpfl		!	L:\users\light\
-the user shares appdata		!	L:\users\light\appdata\
-the user shares desktop		!	L:\users\light\desktop\
-the users share upublic		!	L:\users\public\
-the sysm shares livesys		!	L:\live\
-the sysm shares talkdir		!	L:\live\talk\
-the sysm shares windows		!	L:\windows\
-the sysm shares pg32bit		!	L:\programs\32 bit\
-the sysm shares pg64bit		!	L:\programs\64 bit\
-the sysm shares pgmdata		!	L:\programs\data\
-the sysm shares curtime		!	time
-the sysm shares curdate		!	date
-the users share systemu		!	whoami
-the users share sysinfo		!	systeminfo
 logic cnflcts = none noted to date
 logic sltions = none noted to date
