aca471f182281f827ded511a151f801833d4095b
L Kira <112792604+l-urk@users.noreply.github.com>
2024-07-09 23:06:50 -0700

    Create shares.cmd

---
+++ shares.cmd
@@ -0,0 +1,14 @@
+the user shares userpfl		!	L:\users\light\
+the user shares appdata		!	L:\users\light\appdata\
+the user shares desktop		!	L:\users\light\desktop\
+the users share upublic		!	L:\users\public\
+the sysm shares livesys		!	L:\live\
+the sysm shares talkdir		!	L:\live\talk\
+the sysm shares windows		!	L:\windows\
+the sysm shares pg32bit		!	L:\programs\32 bit\
+the sysm shares pg64bit		!	L:\programs\64 bit\
+the sysm shares pgmdata		!	L:\programs\data\
+the sysm shares curtime		!	time
+the sysm shares curdate		!	date
+the users share systemu		!	whoami
+the users share sysinfo		!	systeminfo
