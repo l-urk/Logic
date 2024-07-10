43907025f38192403fedbccc3242c025932bc610
L Kira <112792604+l-urk@users.noreply.github.com>
2024-07-09 22:06:43 -0700

    Update and rename LSDOS.cmd to LS-DOS.cmd

---
+++ LS-DOS.cmd
@@ -0,0 +1,21 @@
+LS-DOS (Light-Standard Disk Operating System).
+The LSD (Live System Drive) disk and software.
+All default LS-DOS commands operate as normal.
+The LSD disks software will function as such :
+The main drive will be chosen by letter during intial setup. The default main drive letter is "L:".
+Inputing and executing with the environment variable %LSD% will return the current main drive letter.
+The directory structure for the main drive is as follows:
+``
+L:\
+L:\Live\
+L:\Live\System32\
+L:\Programs\
+L:\Programs\32 bit\
+L:\Programs\64 bit\
+L:\Programs\Data\
+L:\Users\Light\
+L:\Users\Light\AppData\
+L:\Users\Light\AppData\Local\
+L:\Users\Light\AppData\LocalLow\
+L:\Users\Light\AppData\Roaming\
+``
