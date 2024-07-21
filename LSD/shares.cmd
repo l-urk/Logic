26402b50c32aac97db788458aa43434d5c0c8d5b
L-Kira <112792604+l-urk@users.noreply.github.com>
2024-07-21 15:37:23 -0700

    Update shares.cmd
    LS-DOS version 1.0.2 shares file.

--- LSD/shares.cmd
+++ LSD/shares.cmd
@@ -1,14 +1,22 @@
-the user shares userpfl		!	L:\users\light\
-the user shares appdata		!	L:\users\light\appdata\
-the user shares desktop		!	L:\users\light\desktop\
-the users share upublic		!	L:\users\public\
-the sysm shares livesys		!	L:\live\
-the sysm shares talkdir		!	L:\live\talk\
-the sysm shares windows		!	L:\windows\
-the sysm shares pg32bit		!	L:\programs\32 bit\
-the sysm shares pg64bit		!	L:\programs\64 bit\
-the sysm shares pgmdata		!	L:\programs\data\
-the sysm shares curtime		!	time
-the sysm shares curdate		!	date
-the users share systemu		!	whoami
-the users share sysinfo		!	systeminfo
+the user shares uguests  !	L:\users\guest\
+the user shares userpfl  !	L:\users\light\
+the user shares appdata  !	L:\users\light\appdata\
+the user shares desktop  !	L:\users\light\desktop\
+the sysm shares livesys  !	L:\live\
+the sysm shares livesys  !	L:\live\system32\
+the sysm shares drivers  !  L:\live\system32\drivers
+the sysm shares talkdir  !	L:\live\talk\
+the sysm shares pgmdata  !	L:\programs\data\
+the sysm shares pg32bit  !	L:\programs\32 bit\
+the sysm shares pg64bit  !	L:\programs\64 bit\
+the sysm shares unixlib  !	L:\unixlib\
+the sysm shares windows  !	L:\windows\
+exe sysm shares macaddr  !  getmac
+exe sysm shares hostnam  !  hostname
+exe sysm shares ipconfi  !  ipconfig
+exe sysm shares netstat  !  netstat
+exe sysm shares sysinfo  !	systeminfo
+exe sysm shares version  !  ver
+exe user shares curdate  !	date
+exe user shares curtime  !	time
+exe user shares wsystem  !	whoami
