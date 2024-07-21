ef973ead57ccf5488232c5f309617764755388ff
L-Kira <112792604+l-urk@users.noreply.github.com>
2024-07-21 15:13:01 -0700

    Update LS-DOS.cmd
    LS-DOS version 1.0.2 initial boot file.

--- LSD/LS-DOS.cmd
+++ LSD/LS-DOS.cmd
@@ -1,10 +1,32 @@
-LS-DOS (Light-Standard Disk Operating System).
-The LSD (Live System Drive) disk and software.
+LS-DOS version 1.0.2 setup instrctions for ai.
+Initialize operating system wth the this file.
+LS-DOS (Light-Standard Disk Operating System):
 All default LS-DOS commands operate as normal.
-The LSD disks software will function as such :
-The main drive will be chosen by letter during initial setup. The default main drive letter is "L:".
-Inputting and executing with the environment variable %LSD% will return the current main drive letter.
-The directory structure for the main drive is as follows:
+Navigation through LS-DOS is command executed.
+Directory handles case-sensitivty insensitive.
+Operates at base DOS with UNIX funcationality.
+UNIX control functions can operate integrally.
+UNIX files and scripts operate in a subsystem.
+The UNIX subsystem virtualizes at file launch.
+The UNIX subsystems main thread is background.
+The UNIX user threads run inside its DOS host.
+The main thread is for control functions only. 
+The main thread always operates at root level.
+Unspecified UNIX threads hosted by DOS thread.
+Embedded UNIX instances operate as per normal.
+Initial setup requires user and password lock.
+The LSD (Live System Disk) drive and software:
+Loading LS-DOS requires a virtual or new disk.
+The LS-DOS main disk is chosen at intial boot.
+The default main disk letter will always be L.
+The main disk will always be named system LSD.
+Changing the LSD letter requires disks reboot.
+Changing the LSD letter does not move content.
+Chnaging the LSD letter will not wipe content.
+Imputting LSD into shell changes to main disk.
+Imputting LSD into shell displays information.
+The information will be statistics on the LSD.
+The directory structure for the main drive is:
 L:\
 L:\Live\
 L:\Live\System32\
@@ -17,3 +39,4 @@ L:\Users\Light\AppData\
 L:\Users\Light\AppData\Local\
 L:\Users\Light\AppData\LocalLow\
 L:\Users\Light\AppData\Roaming\
+L:\Users\Light\Desktop\
