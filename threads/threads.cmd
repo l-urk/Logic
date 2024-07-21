c9c125ac6be51635fccbfc66b226803d358c2f5d
L-Kira <112792604+l-urk@users.noreply.github.com>
2024-07-21 16:18:07 -0700

    Update threads.cmd

--- threads/threads.cmd
+++ threads/threads.cmd
@@ -1,26 +1,28 @@
-a0~az
-b0~bz
-c0~cz
-d0~dz
-e0~ez
+reserved_apex_threads = a0~az
+reserved_beta_therads = b0~bz
+reserved_cash_threads = c0~cz
+reserved_date_threads = d0~dz
+reserved_epic_threads = e0~ez
 reserved_flag_threads = f0~fz
-g0~gz
-h0~hz
-i0~iz
-j0~jz
-k0~kz
+reserved_gear_threads = g0~gz
+reserved_hell_threads = h0~hz
+reserved_info_threads = i0~iz
+reserved_join_threads = j0~jz
+reserved_kill_threads = k0~kz
 reserved_live_threads = l0~lz
-m0~mz
-n0~nz
-o0~oz
-p0~pz
+reserved_mind_threads = m0~mz
+reserved_nine_threads = n0~nz
+reserved_open_threads = o0~oz
+reserved_pipe_threads = p0~pz
+|
 reserved_qtum_threads = q0~qz
-reserved_rand_threads = r0~rz
+reserved_rndm_threads = r0~rz
 reserved_syst_threads = s0~sz
+|
 reserved_time_threads = t0~tz
 reserved_user_threads = u0~uz
-v0~vz
-w0~wz
-reserved_real_threads = x0~xz
-reserved_side_threads = y0~yz
-reserved_base_threads = z0~zz
+reserved_void_threads = v0~vz
+reserved_wire_threads = w0~wz
+reserved_xena_threads = x0~xz
+reserved_year_threads = y0~yz
+reserved_zeds_threads = z0~zz
