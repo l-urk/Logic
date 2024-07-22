ba6f2feeaf2b723cb270a198b5d1a869f7bbc200
L-Kira <112792604+l-urk@users.noreply.github.com>
2024-07-21 17:07:14 -0700

    Update threads.cmd

--- THREADS/threads.cmd
+++ THREADS/threads.cmd
@@ -1,3 +1,34 @@
+a0~zz 
+{
+a0~az
+b0~bz
+c0~cz
+d0~dz
+e0~ez
+f0~fz
+g0~gz
+h0~hz
+i0~iz
+j0~jz
+k0~kz
+l0~lz
+m0~mz
+n0~nz
+o0~oz
+p0~pz
+q0~qz
+r0~rz
+s0~sz
+t0~tz
+u0~uz
+v0~vz
+w0~wz
+x0~xz
+y0~yz
+z0~zz 
+}
+reserved_thread_names = a0~zz
+{
 reserved_apex_threads = a0~az
 reserved_beta_therads = b0~bz
 reserved_cash_threads = c0~cz
@@ -14,11 +45,9 @@ reserved_mind_threads = m0~mz
 reserved_nine_threads = n0~nz
 reserved_open_threads = o0~oz
 reserved_pipe_threads = p0~pz
-|
-reserved_qtum_threads = q0~qz
+reserved_qntm_threads = q0~qz
 reserved_rndm_threads = r0~rz
 reserved_syst_threads = s0~sz
-|
 reserved_time_threads = t0~tz
 reserved_user_threads = u0~uz
 reserved_void_threads = v0~vz
@@ -26,3 +55,4 @@ reserved_wire_threads = w0~wz
 reserved_xena_threads = x0~xz
 reserved_year_threads = y0~yz
 reserved_zeds_threads = z0~zz
+}
